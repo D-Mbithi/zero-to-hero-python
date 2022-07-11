51dadf378a0b259cb3baec0d2b47f0f58d6dba54
dennis <denmbithi@gmail.com>
2022-07-11 09:27:51 +0300

    age calculater script

---
+++ age.py
@@ -0,0 +1,5 @@
+year_born = int(input("Enter the year you were born? "))
+
+age = 2022 - year_born
+
+print(f"You are {age} years old.")
--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # Hello world script
-#
+
 
 print("Hello, world!")
