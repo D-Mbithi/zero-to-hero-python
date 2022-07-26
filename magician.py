c4bba774aafeb17024ddbde0ae7e9df1bc2db4b6
dennis <denmbithi@gmail.com>
2022-07-26 14:27:40 +0300

    if statement demo script

---
+++ magician.py
@@ -0,0 +1,12 @@
+is_magician = True
+is_expert = False
+
+if is_magician and is_expert:
+    print("You are a master")
+
+elif not is_expert and is_magician:
+    print("At least you are getting there.")
+
+elif not is_magician:
+    print("You need magic")
+
